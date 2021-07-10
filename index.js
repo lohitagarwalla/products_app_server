8c88879fa668ecc0661291482615f1d6818dfead
lohitagarwalla <lohitagarwalla@gmail.com>
2021-07-10 22:01:30 +0530

    Connect mongodb
    Signed-off-by: lohitagarwalla <lohitagarwalla@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const mongodburl = require('./source/config/file')
 
 const app = express()
+mongoose.connect(mongodburl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log('Mongodb connected')
+    })
+    .catch(e => {
+        console.log(e)
+        console.log('Error in connection mongodb')
+    })
+
 
 const port = 3000
 
