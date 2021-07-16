7995b732ffa0c0ab51c5a882f8a807c8ffa8ae76
Lohit Agarwalla <lohitagarwalla@gmail.com>
2021-07-16 19:49:54 +0530

    try to upload large file
    Signed-off-by: Lohit Agarwalla <lohitagarwalla@gmail.com>

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const Product = require('./src/model/product')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json())
+// app.use(express.json())
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 app.get('/', (req, res) => {
     res.send('Hello world')
